8fed88025974b02d14edd0c74849ae2df9e617e1
Camille <mc.mouronval@gmail.com>
2025-12-05 01:04:07 +0100

    feat(web): add select on main page

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,43 @@
 <template>
-	<div>
-		<p>{{ $t('hello') }}</p>
-	</div>
+	<label for="select-list">Select<br></label>
+
+	<select
+		id="compo-select"
+		v-model="optionSelected"
+		name="compo"
+	>
+		<option value="">
+			--Choisir une option--
+		</option>
+		<option value="compo1">
+			Composant1
+		</option>
+		<option value="compo2">
+			Composant2
+		</option>
+		<option value="compo3">
+			Composant3
+		</option>
+		<option value="compo4">
+			Composant4
+		</option>
+		<option value="compo5">
+			Composant5
+		</option>
+		<option value="compo6">
+			Composant6
+		</option>
+	</select>
+
+	{{ optionSelected }}
 </template>
+
+<script setup>
+const optionSelected = ref('');
+</script>
+
+<style>
+h1 {
+	color: #42b983;
+}
+</style>
