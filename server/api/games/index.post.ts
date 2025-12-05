ab71090689e3ca35410e77633388ff3b4ae88549
NockXu <gabriel.dav@outlook.fr>
2025-12-05 01:33:32 +0100

    feat(store): Ajout des interaction avec la base

--- server/api/games/index.post.ts
+++ server/api/games/index.post.ts
@@ -5,11 +5,15 @@ export default defineEventHandler(async (event) => {
 	const user = event.context.user;
 	const { title, timer } = await readBody(event);
 
+	if (!title || !timer) {
+		throw createError({ statusCode: 400, message: 'Titre et timer requis' });
+	}
+
 	return prisma.game.create({
 		data: {
 			title,
 			timer,
-			date: new Date(),
+			date: new Date(), // Date générée côté serveur
 			userId: user.id,
 		},
 	});
