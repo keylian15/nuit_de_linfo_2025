83c2b4e293db4bdafa4afd79016e07c704e315a5
flosenicourt <florian.senicourt@etu.univ-littoral.fr>
2025-12-05 01:01:02 +0100

    feat(web): Add checkbox on main page

---
+++ app/components/choice-checkbox.vue
@@ -0,0 +1,43 @@
+<template>
+	<div>
+		<li>
+			<input
+				v-model="checking"
+				type="checkbox"
+				value="text1"
+				class="check1"
+			>
+			<label for="check1" />
+			<input
+				v-model="checking"
+				type="checkbox"
+				value="text2"
+				class="check2"
+			>
+			<label for="check2" />
+		</li>
+	</div>
+	{{ text1 }}
+	{{ text2 }}
+</template>
+
+<script setup>
+const checking = ref([]);
+const text1 = ref('');
+const text2 = ref('');
+if (text1.value in checking.value) {
+	text1.value = 'text1';
+}
+else {
+	text1.value = '';
+}
+if (text2.value in checking.value) {
+	text2.value = 'text2';
+}
+else {
+	text2.value = '';
+}
+</script>
+
+<style>
+</style>
